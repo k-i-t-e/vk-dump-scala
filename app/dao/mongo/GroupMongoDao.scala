a959e66f50c423e8cf9440209fb0edaaee82691c
Mikhail_Miroliubov <mike.miroliubov@gmail.com>
2018-12-06 17:48:03 +0300

    MongoDB support in progress

--- app/dao/mongo/GroupMongoDao.scala
+++ app/dao/mongo/GroupMongoDao.scala
@@ -6,7 +6,7 @@ import model.{Group, VkUser}
 import play.modules.reactivemongo.ReactiveMongoApi
 import reactivemongo.api.Cursor
 import reactivemongo.api.collections.bson.BSONCollection
-import reactivemongo.bson.{BSONArray, BSONDocument, BSONDocumentReader, BSONDocumentWriter, Macros, document}
+import reactivemongo.bson.{BSONDocument, BSONDocumentReader, BSONDocumentWriter, Macros, document}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -46,7 +46,7 @@ class GroupMongoDao @Inject()(reactiveMongoApi: ReactiveMongoApi)(implicit ec: E
                             .cursor[BSONDocument]()
                             .fold(Seq.empty[Group])((res, doc) => {
                               val group = groupReader.read(doc)
-                              val withUsers = group.withUsers(doc.getAs[List[Long]]("users")
+                              val withUsers = group.withUsers(doc.getAs[Seq[Long]]("users")
                                                                 .map(_.withFilter(u.contains).map(u(_))))
                               res :+ withUsers
                             }) }
@@ -61,21 +61,41 @@ class GroupMongoDao @Inject()(reactiveMongoApi: ReactiveMongoApi)(implicit ec: E
         .cursor[BSONDocument]()
         .fold(Seq.empty[Group])((res, doc) => {
           val group = groupReader.read(doc)
-          val withUsers = group.withUsers(doc.getAs[List[Long]]("users")
+          val withUsers = group.withUsers(doc.getAs[Seq[Long]]("users")
                                             .map(_.withFilter(u.contains).map(u(_))))
           res :+ withUsers
         }) }
     } yield g.headOption
 
-  override def updateGroup(group: Group, userIds: Seq[Long]): Future[_] = ???
+  override def updateGroup(group: Group, userIds: Seq[Long]): Future[_] = groups.flatMap {
+    _.update(document("id" -> group.id), GroupWriter.write(group) ++ document("users" -> userIds))
+  }
 
-  override def updateGroup(group: Group): Future[_] = ???
+  override def updateGroup(group: Group): Future[_] = groups.flatMap {
+    _.findAndUpdate(
+      document("id" -> group.id),
+      document("$set" -> (document(GroupWriter.write(group)) -- "users")))
+  }
 
-  override def insertGroup(group: Group, userIds: Seq[Long]): Future[_] = ???
 
-  override def addGroupUsers(groupId: Long, userIds: Iterable[Long]): Future[_] = ???
+  override def insertGroup(group: Group, userIds: Seq[Long]): Future[_] = groups.flatMap {
+    _.insert(GroupWriter.write(group) ++ document("users" -> userIds))
+  }
 
-  override def removeGroupUsers(groupId: Long, userIds: Seq[Long]): Future[_] = ???
+  override def addGroupUsers(groupId: Long, userIds: Iterable[Long]): Future[_] =
+    updateUsers(groupId, oldIds => oldIds ++ userIds)
+
+  override def removeGroupUsers(groupId: Long, userIds: Seq[Long]): Future[_] =
+    updateUsers(groupId, oldIds => oldIds -- userIds)
+
+
+  private def updateUsers(groupId: Long, userIdsUpdate: Set[Long] => Iterable[Long]) =
+    for {
+      doc <- groups.flatMap(_.find(document("id" -> groupId)).requireOne[BSONDocument])
+      newIds = userIdsUpdate(doc.getAs[Seq[Long]]("users").getOrElse(Seq.empty).toSet)
+      res <- groups.flatMap(_.update(document("id" -> groupId),
+                                     document("$set" -> document("users" -> newIds))))
+    } yield res
 
   override def findGroupsByUser(userId: Long): Future[Seq[Group]] = ???
 
